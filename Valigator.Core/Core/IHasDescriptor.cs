8d80352b5d3392c2507d95cecad4d26f9df9631b
Johannes Moersch <Johannes.Moersch@gmail.com>
2019-05-28 19:16:28 -0600

    Adds interface for accessing descriptor.

---
+++ Valigator.Core/Core/IHasDescriptor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Valigator.Core
+{
+	public interface IHasDescriptor
+	{
+		DataDescriptor DataDescriptor { get; }
+	}
+}
