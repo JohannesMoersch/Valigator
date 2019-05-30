6247016e77c80ae32f84f6a0407a8e43c06ddc45
Johannes Moersch <Johannes.Moersch@gmail.com>
2019-05-29 22:12:07 -0600

    Breaks out some code gen into typed model file.

---
+++ Valigator.Core/Core/Model.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+using Functional;
+using Valigator.Core.Descriptors;
+
+namespace Valigator.Core
+{
+	internal static class Model<TModel>
+	{
+		private static object _getPropertyDescriptorsLockObj = new object(); 
+		private static Func<TModel, PropertyDescriptor[]> _getPropertyDescriptors;
+
+		public static PropertyDescriptor[] GetPropertyDescriptors(TModel model)
+		{
+			if (typeof(TModel).IsPrimitive)
+				return Array.Empty<PropertyDescriptor>();
+
+			if (_getPropertyDescriptors == null)
+			{
+				lock (_getPropertyDescriptorsLockObj)
+				{
+					if (_getPropertyDescriptors == null)
+						_getPropertyDescriptors = CreatePropertyDescriptorsFunction();
+				}
+			}
+
+			return _getPropertyDescriptors.Invoke(model);
+		}
+
+		private static Func<TModel, PropertyDescriptor[]> CreatePropertyDescriptorsFunction()
+		{
+			var modelExpression = Expression.Parameter(typeof(TModel), "model");
+
+			var propertyDescriptors = typeof(TModel)
+				.GetProperties()
+				.Where(property => property.PropertyType.IsConstructedGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Data<>))
+				.Select(property => CreatePropertyDescriptor(modelExpression, property));
+
+			var arrayInitializer = Expression.NewArrayInit(typeof(PropertyDescriptor), propertyDescriptors);
+
+			return Expression.Lambda<Func<TModel, PropertyDescriptor[]>>(arrayInitializer, modelExpression).Compile();
+		}
+
+		private static Expression CreatePropertyDescriptor(Expression modelExpression, PropertyInfo property)
+		{
+			var data = Expression.Property(modelExpression, property);
+
+			var dataDescriptor = Expression.Property(data, nameof(Data<object>.DataDescriptor));
+
+			var constructor = typeof(PropertyDescriptor).GetConstructor(new[] { typeof(string), typeof(Type), typeof(IStateDescriptor), typeof(IEnumerable<IValueDescriptor>) });
+
+			var propertyType = Expression.Property(dataDescriptor, nameof(DataDescriptor.PropertyType));
+
+			var stateDescriptor = Expression.Property(dataDescriptor, nameof(DataDescriptor.StateDescriptor));
+
+			var valueDescriptors = Expression.Property(dataDescriptor, nameof(DataDescriptor.ValueDescriptors));
+
+			return Expression.New(constructor, Expression.Constant(property.Name, typeof(string)), propertyType, stateDescriptor, valueDescriptors);
+		}
+
+		private static object _verifyModelLockObj = new object();
+		private static Func<TModel, ValidationError[]> _verifyModel;
+
+		public static Result<Unit, ValidationError[]> Verify(TModel model)
+		{
+			if (typeof(TModel).IsPrimitive)
+				return Result.Unit<ValidationError[]>();
+
+			if (_verifyModel == null)
+			{
+				lock (_verifyModelLockObj)
+				{
+					if (_verifyModel == null)
+						_verifyModel = CreateVerifyFunction();
+				}
+			}
+
+			var validationErrors = _verifyModel.Invoke(model);
+
+			return Result.Create(!validationErrors.Any(error => error != null), () => Unit.Value, () => validationErrors.OfType<ValidationError>().ToArray());
+		}
+
+		private static Func<TModel, ValidationError[]> CreateVerifyFunction()
+		{
+			var modelParameter = Expression.Parameter(typeof(TModel), "model");
+
+			var modelExpression = Expression.Convert(modelParameter, typeof(TModel));
+
+			var validationErrors = typeof(TModel)
+				.GetProperties()
+				.Where(property => property.PropertyType.IsConstructedGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Data<>))
+				.Select(property => VerifyProperty(modelExpression, property));
+
+			var arrayInitializer = Expression.NewArrayInit(typeof(ValidationError), validationErrors);
+
+			return Expression.Lambda<Func<TModel, ValidationError[]>>(arrayInitializer, modelParameter).Compile();
+		}
+
+		private static Expression VerifyProperty(Expression modelExpression, PropertyInfo property)
+		{
+			var methods = GetVerifySupportMethods(property.PropertyType);
+
+			var dataProperty = Expression.Property(modelExpression, property);
+
+			var result = Expression.Call(dataProperty, methods.verify, modelExpression);
+
+			var isSuccess = Expression.Call(methods.isSuccess, result);
+
+			var getSuccess = Expression.Call(methods.getSuccess, result);
+
+			var getFailure = Expression.Call(methods.getFailure, result);
+
+			var onSuccess = Expression.Block(Expression.Assign(dataProperty, getSuccess), Expression.Constant(null, typeof(ValidationError)));
+
+			return Expression.Condition(isSuccess, onSuccess, getFailure, typeof(ValidationError));
+		}
+
+		private static readonly ConcurrentDictionary<Type, (MethodInfo verify, MethodInfo isSuccess, MethodInfo getSuccess, MethodInfo getFailure)> _getVerifySupportMethods = new ConcurrentDictionary<Type, (MethodInfo verify, MethodInfo isSuccess, MethodInfo getSuccess, MethodInfo getFailure)>();
+
+		private static (MethodInfo verify, MethodInfo isSuccess, MethodInfo getSuccess, MethodInfo getFailure) GetVerifySupportMethods(Type dataType)
+		{
+			if (!_getVerifySupportMethods.TryGetValue(dataType, out var methods))
+			{
+				methods = CreateVerifySupportMethods(dataType);
+
+				_getVerifySupportMethods.TryAdd(dataType, methods);
+			}
+
+			return methods;
+		}
+
+		private static (MethodInfo verify, MethodInfo isSuccess, MethodInfo getSuccess, MethodInfo getFailure) CreateVerifySupportMethods(Type dataType)
+		{
+			var valueType = dataType.GetGenericArguments()[0];
+
+			var verify = dataType.GetMethod(nameof(Data<object>.Verify), new[] { typeof(object) });
+
+			var isSuccess = typeof(Model<TModel>).GetMethod(nameof(IsSuccess), BindingFlags.NonPublic | BindingFlags.Static).MakeGenericMethod(valueType);
+
+			var getSuccess = typeof(Model<TModel>).GetMethod(nameof(GetSuccess), BindingFlags.NonPublic | BindingFlags.Static).MakeGenericMethod(valueType);
+
+			var getFailure = typeof(Model<TModel>).GetMethod(nameof(GetFailure), BindingFlags.NonPublic | BindingFlags.Static).MakeGenericMethod(valueType);
+
+			return (verify, isSuccess, getSuccess, getFailure);
+		}
+
+		private static bool IsSuccess<TValue>(Result<Data<TValue>, ValidationError> result)
+			=> result.Match(_ => true, _ => false);
+
+		private static Data<TValue> GetSuccess<TValue>(Result<Data<TValue>, ValidationError> result)
+			=> result.Match(_ => _, _ => default);
+
+		private static ValidationError GetFailure<TValue>(Result<Data<TValue>, ValidationError> result)
+			=> result.Match(_ => default, _ => _);
+	}
+}
--- Valigator.Core/Model.cs
+++ Valigator.Core/Model.cs
@@ -14,7 +14,7 @@ namespace Valigator.Core
 	{
 		private static readonly ConcurrentDictionary<Type, Func<object, PropertyDescriptor[]>> _getPropertyDescriptorsFunctions = new ConcurrentDictionary<Type, Func<object, PropertyDescriptor[]>>();
 
-		public static PropertyDescriptor[] GetPropertyDescriptors(this object model)
+		public static PropertyDescriptor[] GetPropertyDescriptors(object model)
 		{
 			var modelType = model?.GetType() ?? throw new ArgumentNullException(nameof(model));
 
@@ -32,36 +32,14 @@ namespace Valigator.Core
 		{
 			var modelParameter = Expression.Parameter(typeof(object), "model");
 
-			var modelExpression = Expression.Convert(modelParameter, modelType);
+			var method = typeof(Model<>).MakeGenericType(modelType).GetMethod(nameof(Model<object>.GetPropertyDescriptors));
 
-			var propertyDescriptors = modelType
-				.GetProperties()
-				.Where(property => property.PropertyType.IsConstructedGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Data<>))
-				.Select(property => CreatePropertyDescriptor(modelExpression, property));
+			var expression = Expression.Call(method, Expression.Convert(modelParameter, modelType));
 
-			var arrayInitializer = Expression.NewArrayInit(typeof(PropertyDescriptor), propertyDescriptors);
-
-			return Expression.Lambda<Func<object, PropertyDescriptor[]>>(arrayInitializer, modelParameter).Compile();
+			return Expression.Lambda<Func<object, PropertyDescriptor[]>>(expression, modelParameter).Compile();
 		}
 
-		private static Expression CreatePropertyDescriptor(Expression modelExpression, PropertyInfo property)
-		{
-			var data = Expression.Property(modelExpression, property);
-
-			var dataDescriptor = Expression.Property(data, nameof(Data<object>.DataDescriptor));
-
-			var constructor = typeof(PropertyDescriptor).GetConstructor(new[] { typeof(string), typeof(Type), typeof(IStateDescriptor), typeof(IEnumerable<IValueDescriptor>) });
-
-			var propertyType = Expression.Property(dataDescriptor, nameof(DataDescriptor.PropertyType));
-
-			var stateDescriptor = Expression.Property(dataDescriptor, nameof(DataDescriptor.StateDescriptor));
-
-			var valueDescriptors = Expression.Property(dataDescriptor, nameof(DataDescriptor.ValueDescriptors));
-
-			return Expression.New(constructor, Expression.Constant(property.Name, typeof(string)), propertyType, stateDescriptor, valueDescriptors);
-		}
-
-		private static readonly ConcurrentDictionary<Type, Func<object, ValidationError[]>> _verifyModelFunctions = new ConcurrentDictionary<Type, Func<object, ValidationError[]>>();
+		private static readonly ConcurrentDictionary<Type, Func<object, Result<Unit, ValidationError[]>>> _verifyModelFunctions = new ConcurrentDictionary<Type, Func<object, Result<Unit, ValidationError[]>>>();
 
 		public static Result<Unit, ValidationError[]> Verify(object model)
 		{
@@ -74,82 +52,18 @@ namespace Valigator.Core
 				_verifyModelFunctions.TryAdd(modelType, function);
 			}
 
-			var validationErrors = function.Invoke(model);
-
-			return Result.Create(!validationErrors.Any(error => error != null), () => Unit.Value, () => validationErrors.OfType<ValidationError>().ToArray());
+			return function.Invoke(model);
 		}
 
-		public static Func<object, ValidationError[]> CreateVerifyFunction(Type modelType)
+		private static Func<object, Result<Unit, ValidationError[]>> CreateVerifyFunction(Type modelType)
 		{
 			var modelParameter = Expression.Parameter(typeof(object), "model");
 
-			var modelExpression = Expression.Convert(modelParameter, modelType);
+			var method = typeof(Model<>).MakeGenericType(modelType).GetMethod(nameof(Model<object>.Verify));
 
-			var validationErrors = modelType
-				.GetProperties()
-				.Where(property => property.PropertyType.IsConstructedGenericType && property.PropertyType.GetGenericTypeDefinition() == typeof(Data<>))
-				.Select(property => VerifyProperty(modelExpression, property));
+			var expression = Expression.Call(method, Expression.Convert(modelParameter, modelType));
 
-			var arrayInitializer = Expression.NewArrayInit(typeof(ValidationError), validationErrors);
-
-			return Expression.Lambda<Func<object, ValidationError[]>>(arrayInitializer, modelParameter).Compile();
+			return Expression.Lambda<Func<object, Result<Unit, ValidationError[]>>>(expression, modelParameter).Compile();
 		}
-
-		private static Expression VerifyProperty(Expression modelExpression, PropertyInfo property)
-		{
-			var methods = GetVerifySupportMethods(property.PropertyType);
-
-			var dataProperty = Expression.Property(modelExpression, property);
-
-			var result = Expression.Call(dataProperty, methods.verify, modelExpression);
-
-			var isSuccess = Expression.Call(methods.isSuccess, result);
-
-			var getSuccess = Expression.Call(methods.getSuccess, result);
-
-			var getFailure = Expression.Call(methods.getFailure, result);
-
-			var onSuccess = Expression.Block(Expression.Assign(dataProperty, getSuccess), Expression.Constant(null, typeof(ValidationError)));
-
-			return Expression.Condition(isSuccess, onSuccess, getFailure, typeof(ValidationError));
-		}
-
-		private static readonly ConcurrentDictionary<Type, (MethodInfo verify, MethodInfo isSuccess, MethodInfo getSuccess, MethodInfo getFailure)> _getVerifySupportMethods = new ConcurrentDictionary<Type, (MethodInfo verify, MethodInfo isSuccess, MethodInfo getSuccess, MethodInfo getFailure)>();
-
-		private static (MethodInfo verify, MethodInfo isSuccess, MethodInfo getSuccess, MethodInfo getFailure) GetVerifySupportMethods(Type dataType)
-		{
-			if (!_getVerifySupportMethods.TryGetValue(dataType, out var methods))
-			{
-				methods = CreateVerifySupportMethods(dataType);
-
-				_getVerifySupportMethods.TryAdd(dataType, methods);
-			}
-
-			return methods;
-		}
-
-		private static (MethodInfo verify, MethodInfo isSuccess, MethodInfo getSuccess, MethodInfo getFailure) CreateVerifySupportMethods(Type dataType)
-		{
-			var valueType = dataType.GetGenericArguments()[0];
-
-			var verify = dataType.GetMethod(nameof(Data<object>.Verify), new[] { typeof(object) });
-
-			var isSuccess = typeof(Model).GetMethod(nameof(IsSuccess), BindingFlags.NonPublic | BindingFlags.Static).MakeGenericMethod(valueType);
-
-			var getSuccess = typeof(Model).GetMethod(nameof(GetSuccess), BindingFlags.NonPublic | BindingFlags.Static).MakeGenericMethod(valueType);
-
-			var getFailure = typeof(Model).GetMethod(nameof(GetFailure), BindingFlags.NonPublic | BindingFlags.Static).MakeGenericMethod(valueType);
-
-			return (verify, isSuccess, getSuccess, getFailure);
-		}
-
-		private static bool IsSuccess<TValue>(Result<Data<TValue>, ValidationError> result)
-			=> result.Match(_ => true, _ => false);
-
-		private static Data<TValue> GetSuccess<TValue>(Result<Data<TValue>, ValidationError> result)
-			=> result.Match(_ => _, _ => default);
-
-		private static ValidationError GetFailure<TValue>(Result<Data<TValue>, ValidationError> result)
-			=> result.Match(_ => default, _ => _);
 	}
 }
