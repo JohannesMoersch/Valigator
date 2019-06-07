4b727da69666988711d71e91d87702343b59db4c
Johannes Moersch <Johannes.Moersch@gmail.com>
2019-06-06 23:31:43 -0600

    Adds Valigator.AspNetCore project with support for asp.net core.

---
+++ Valigator.AspNetCore/MvcBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Valigator.AspNetCore;
+using Valigator.Newtonsoft.Json;
+
+namespace Valigator
+{
+	public static class MvcBuilderExtensions
+	{
+		public static IMvcBuilder AddValigator(this IMvcBuilder builder)
+			=> builder
+				.AddMvcOptions(options => options.Filters.Add(new ValigatorFilter()))
+				.AddJsonOptions(options => options.SerializerSettings.Converters.Add(new ValigatorConverter()));
+	}
+}
---
+++ Valigator.AspNetCore/ValigatorFilter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Functional;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Valigator.Core;
+
+namespace Valigator.AspNetCore
+{
+	public class ValigatorFilter : IActionFilter, IOrderedFilter
+	{
+		public int Order => -2500;
+
+		public void OnActionExecuted(ActionExecutedContext context)
+		{
+		}
+
+		public void OnActionExecuting(ActionExecutingContext context)
+		{
+			foreach (var kvp in context.ActionArguments)
+			{
+				Model
+					.Verify(kvp.Value)
+					.Match
+					(
+						_ => _,
+						errors =>
+						{
+							context.ModelState.AddModelError(kvp.Key, String.Join(", ", errors.Select(e => e.Message)));
+
+							return Unit.Value;
+						}
+					);
+			}
+		}
+	}
+}
