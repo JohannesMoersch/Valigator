9ecdc05909b608bc8e52ee21435cb69f6b6c879d
Johannes Moersch <Johannes.Moersch@gmail.com>
2019-06-11 23:53:32 -0600

    Adds ability to get descriptor from validate attribute.

--- Valigator.Tests/AspNetCore/ValidateAttributeTests.cs
+++ Valigator.Tests/AspNetCore/ValidateAttributeTests.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using FluentAssertions;
+using Valigator.AspNetCore;
 using Xunit;
 
 namespace Valigator.Tests.AspNetCore
@@ -19,5 +20,16 @@ namespace Valigator.Tests.AspNetCore
 			=> new TestValidateAttribute()
 				.Verify(typeof(int), 0)
 				.AssertSuccess();
+
+		[Fact]
+		public void Test2()
+			=> Assert.Throws<ValidateAttributeDoesNotSupportTypeException>(() => new TestValidateAttribute().Verify(typeof(float), 1.0f));
+
+		[Fact]
+		public void Test3()
+		{
+			var descriptor = new TestValidateAttribute()
+				.GetDescriptor(typeof(int));
+		}
 	}
 }
