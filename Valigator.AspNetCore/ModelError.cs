6d139b3ba35bdb4f1d29cc23ad362daea1b3e65c
Johannes Moersch <Johannes.Moersch@gmail.com>
2019-06-12 21:24:47 -0600

    Adds name and constructor to model error.

--- Valigator.AspNetCore/ModelError.cs
+++ Valigator.AspNetCore/ModelError.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Functional;
 
 namespace Valigator.AspNetCore
 {
 	public class ModelError
 	{
+		public Option<string> Name { get; }
+
 		public ModelSource Source { get; }
 
 		public ValidationError ValidationError { get; }
+
+		public ModelError(Option<string> name, ModelSource source, ValidationError validationError)
+		{
+			Name = name;
+			Source = source;
+			ValidationError = validationError ?? throw new ArgumentNullException(nameof(validationError));
+		}
 	}
 }
