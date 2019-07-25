0c3bab0b3c82b1b55ae7fc205edee4c8b2a0eab8
Johannes Moersch <Johannes.Moersch@gmail.com>
2019-07-25 12:38:09 -0600

    Adds ValidateContentsAttribute and associated unit tests.

---
+++ Valigator.Core/ValidateContentsAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Valigator
+{
+	[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
+	public class ValidateContentsAttribute : Attribute
+	{
+	}
+}
