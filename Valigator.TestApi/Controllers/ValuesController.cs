9102c0010c0c7dbe3cd64a1318029586fd8d40d4
Nathan Magnus <nathanm@iqmetrix.com>
2019-07-25 13:09:01 -0600

    fix

--- Valigator.TestApi/Controllers/ValuesController.cs
+++ Valigator.TestApi/Controllers/ValuesController.cs
@@ -66,7 +66,7 @@ namespace Valigator.TestApi.Controllers
 		}
 
 		// GET api/values/5
-		[HttpGet("test/{id}")]
+		[HttpGet("test/{value}")]
 		public ActionResult<string> Get([Test]int value, [Test]int stuff)
 		{
 			return "value";
