e3d480dffd4c5195880b72e774ea29b694b1d14b
Johannes Moersch <Johannes.Moersch@gmail.com>
2019-09-03 22:51:39 -0600

    Fixes compilation errors in test project.

--- Valigator.Tests/MappingTests.cs
+++ Valigator.Tests/MappingTests.cs
@@ -2,39 +2,54 @@
 using Functional;
 using Valigator.Core;
 using Valigator.Core.StateValidators;
+using Valigator.Core.ValueDescriptors;
 using Xunit;
 
 namespace Valigator.Tests
 {
 	public class MappingTests
 	{
+		private static readonly ValidationError[] _testErrors = new[] { new ValidationError("Test Error", new CustomDescriptor("Test Description")) };
+
 		public class Required
 		{
 			private static RequiredStateValidator<T> CreateData<T>() => Data.Required<T>();
 
 			[Fact]
 			public void InvalidBecauseFailureResult()
-				=> CreateData<int>().Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof(int), typeof(int))), -1).InRange(1).Data.WithValue(500).Verify(new object()).State.Should().Be(DataState.Invalid);
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Failure<float, ValidationError[]>(_testErrors))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.BeEquivalentTo(_testErrors);
 
 			[Fact]
-			public void InvalidBecauseFailureResultAndNoDefault()
-				=> CreateData<int>().Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof(int), typeof(int)))).InRange(1).Data.WithValue(500).Verify(new object()).State.Should().Be(DataState.Invalid);
+			public void InvalidBecauseSourceValidations()
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Success<float, ValidationError[]>(x + 0.5f), o => o.InSet(100))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.OnlyContain(e => e.ValueDescriptor.Equals(new InSetDescriptor(new object[] { 100 })));
 
 			[Fact]
 			public void ValidBecauseSuccessResult()
-				=> CreateData<(int Item1, int Item2)>().Map(x => Result.Success<int, ValidationError>(50), -1).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void ValidBecauseSuccessResultWhenNoDefault()
-				=> CreateData<(int Item1, int Item2)>().Map(x => Result.Success<int, ValidationError>(50)).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void ValidBecauseNormal()
-				=> CreateData<(int Item1, int Item2)>().Map(x => 50).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void InvalidDespiteDefaultIsValid()
-				=> CreateData<(int Item1, int Item2)>().Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof((int, int)), typeof(int))), 50).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Invalid);
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Success<float, ValidationError[]>(x + 0.5f))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.BeEquivalentTo(_testErrors);
 		}
 
 		public class RequiredNullable
@@ -43,27 +58,39 @@ namespace Valigator.Tests
 
 			[Fact]
 			public void InvalidBecauseFailureResult()
-				=> CreateData<int>().Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof(int), typeof(int))), -1).InRange(1).Data.WithValue(500).Verify(new object()).State.Should().Be(DataState.Invalid);
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Failure<float, ValidationError[]>(_testErrors))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.BeEquivalentTo(_testErrors);
 
 			[Fact]
-			public void InvalidBecauseFailureResultAndNoDefault()
-				=> CreateData<int>().Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof(int), typeof(int)))).InRange(1).Data.WithValue(500).Verify(new object()).State.Should().Be(DataState.Invalid);
+			public void InvalidBecauseSourceValidations()
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Success<float, ValidationError[]>(x + 0.5f), o => o.InSet(100))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.OnlyContain(e => e.ValueDescriptor.Equals(new InSetDescriptor(new object[] { 100 })));
 
 			[Fact]
 			public void ValidBecauseSuccessResult()
-				=> CreateData<(int Item1, int Item2)>().Map(x => Result.Success<int, ValidationError>(50), -1).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void ValidBecauseSuccessResultWhenNoDefault()
-				=> CreateData<(int Item1, int Item2)>().Map(x => Result.Success<int, ValidationError>(50)).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void ValidBecauseNormal()
-				=> CreateData<(int Item1, int Item2)>().Map(x => 50).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void InvalidDespiteDefaultIsValid()
-				=> CreateData<(int Item1, int Item2)>().Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof((int, int)), typeof(int))), 50).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Invalid);
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Success<float, ValidationError[]>(x + 0.5f))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.BeEquivalentTo(_testErrors);
 		}
 
 		public class Optional
@@ -72,27 +99,39 @@ namespace Valigator.Tests
 
 			[Fact]
 			public void InvalidBecauseFailureResult()
-				=> CreateData<int>().Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof(int), typeof(int))), -1).InRange(1).Data.WithValue(500).Verify(new object()).State.Should().Be(DataState.Invalid);
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Failure<float, ValidationError[]>(_testErrors))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.BeEquivalentTo(_testErrors);
 
 			[Fact]
-			public void InvalidBecauseFailureResultAndNoDefault()
-				=> CreateData<int>().Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof(int), typeof(int)))).InRange(1).Data.WithValue(500).Verify(new object()).State.Should().Be(DataState.Invalid);
+			public void InvalidBecauseSourceValidations()
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Success<float, ValidationError[]>(x + 0.5f), o => o.InSet(100))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.OnlyContain(e => e.ValueDescriptor.Equals(new InSetDescriptor(new object[] { 100 })));
 
 			[Fact]
 			public void ValidBecauseSuccessResult()
-				=> CreateData<(int Item1, int Item2)>().Map(x => Result.Success<int, ValidationError>(50), -1).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void ValidBecauseSuccessResultWhenNoDefault()
-				=> CreateData<(int Item1, int Item2)>().Map(x => Result.Success<int, ValidationError>(50)).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void ValidBecauseNormal()
-				=> CreateData<(int Item1, int Item2)>().Map(x => 50).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void InvalidDespiteDefaultIsValid()
-				=> CreateData<(int Item1, int Item2)>().Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof((int, int)), typeof(int))), 50).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Invalid);
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Success<float, ValidationError[]>(x + 0.5f))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.BeEquivalentTo(_testErrors);
 		}
 
 		public class OptionalNullable
@@ -101,85 +140,121 @@ namespace Valigator.Tests
 
 			[Fact]
 			public void InvalidBecauseFailureResult()
-				=> CreateData<int>().Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof(int), typeof(int))), -1).InRange(1).Data.WithValue(500).Verify(new object()).State.Should().Be(DataState.Invalid);
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Failure<float, ValidationError[]>(_testErrors))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.BeEquivalentTo(_testErrors);
 
 			[Fact]
-			public void InvalidBecauseFailureResultAndNoDefault()
-				=> CreateData<int>().Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof(int), typeof(int)))).InRange(1).Data.WithValue(500).Verify(new object()).State.Should().Be(DataState.Invalid);
+			public void InvalidBecauseSourceValidations()
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Success<float, ValidationError[]>(x + 0.5f), o => o.InSet(100))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.OnlyContain(e => e.ValueDescriptor.Equals(new InSetDescriptor(new object[] { 100 })));
 
 			[Fact]
 			public void ValidBecauseSuccessResult()
-				=> CreateData<(int Item1, int Item2)>().Map(x => Result.Success<int, ValidationError>(50), -1).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void ValidBecauseSuccessResultWhenNoDefault()
-				=> CreateData<(int Item1, int Item2)>().Map(x => Result.Success<int, ValidationError>(50)).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void ValidBecauseNormal()
-				=> CreateData<(int Item1, int Item2)>().Map(x => 50).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void InvalidDespiteDefaultIsValid()
-				=> CreateData<(int Item1, int Item2)>().Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof((int, int)), typeof(int))), 50).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Invalid);
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Success<float, ValidationError[]>(x + 0.5f))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.BeEquivalentTo(_testErrors);
 		}
 
 		public class Defaulted
 		{
-			private static DefaultedStateValidator<T> CreateData<T>(T value) => Data.Defaulted<T>(value);
+			private static DefaultedStateValidator<T> CreateData<T>() => Data.Defaulted(default(T));
 
 			[Fact]
 			public void InvalidBecauseFailureResult()
-				=> CreateData<int>(7).Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof(int), typeof(int))), -1).InRange(1).Data.WithValue(500).Verify(new object()).State.Should().Be(DataState.Invalid);
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Failure<float, ValidationError[]>(_testErrors))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.BeEquivalentTo(_testErrors);
 
 			[Fact]
-			public void InvalidBecauseFailureResultAndNoDefault()
-				=> CreateData<int>(7).Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof(int), typeof(int)))).InRange(1).Data.WithValue(500).Verify(new object()).State.Should().Be(DataState.Invalid);
+			public void InvalidBecauseSourceValidations()
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Success<float, ValidationError[]>(x + 0.5f), o => o.InSet(100))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.OnlyContain(e => e.ValueDescriptor.Equals(new InSetDescriptor(new object[] { 100 })));
 
 			[Fact]
 			public void ValidBecauseSuccessResult()
-				=> CreateData<(int Item1, int Item2)>((7, 7)).Map(x => Result.Success<int, ValidationError>(50), -1).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void ValidBecauseSuccessResultWhenNoDefault()
-				=> CreateData<(int Item1, int Item2)>((7, 7)).Map(x => Result.Success<int, ValidationError>(50)).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void ValidBecauseNormal()
-				=> CreateData<(int Item1, int Item2)>((7, 7)).Map(x => 50).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void InvalidDespiteDefaultIsValid()
-				=> CreateData<(int Item1, int Item2)>((7,7)).Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof((int, int)), typeof(int))), 50).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Invalid);
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Success<float, ValidationError[]>(x + 0.5f))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.BeEquivalentTo(_testErrors);
 		}
 
 		public class DefaultedNullable
 		{
-			private static NullableDefaultedStateValidator<T> CreateData<T>(T value) => Data.Defaulted<T>(value).Nullable();
+			private static NullableDefaultedStateValidator<T> CreateData<T>() => Data.Defaulted(default(T)).Nullable();
 
 			[Fact]
 			public void InvalidBecauseFailureResult()
-				=> CreateData<int>(7).Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof(int), typeof(int))), -1).InRange(1).Data.WithValue(500).Verify(new object()).State.Should().Be(DataState.Invalid);
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Failure<float, ValidationError[]>(_testErrors))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.BeEquivalentTo(_testErrors);
 
 			[Fact]
-			public void InvalidBecauseFailureResultAndNoDefault()
-				=> CreateData<int>(7).Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof(int), typeof(int)))).InRange(1).Data.WithValue(500).Verify(new object()).State.Should().Be(DataState.Invalid);
+			public void InvalidBecauseSourceValidations()
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Success<float, ValidationError[]>(x + 0.5f), o => o.InSet(100))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.OnlyContain(e => e.ValueDescriptor.Equals(new InSetDescriptor(new object[] { 100 })));
 
 			[Fact]
 			public void ValidBecauseSuccessResult()
-				=> CreateData<(int Item1, int Item2)>((7, 7)).Map(x => Result.Success<int, ValidationError>(50), -1).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void ValidBecauseSuccessResultWhenNoDefault()
-				=> CreateData<(int Item1, int Item2)>((7, 7)).Map(x => Result.Success<int, ValidationError>(50)).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void ValidBecauseNormal()
-				=> CreateData<(int Item1, int Item2)>((7, 7)).Map(x => 50).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Valid);
-
-			[Fact]
-			public void InvalidDespiteDefaultIsValid()
-				=> CreateData<(int Item1, int Item2)>((7, 7)).Map(x => Result.Failure<int, ValidationError>(MappingError.Create("An error for the ages", typeof((int, int)), typeof(int))), 50).InRange(1).Data.WithValue((100, 200)).Verify(new object()).State.Should().Be(DataState.Invalid);
+				=> CreateData<float>()
+					.MappedFrom<int>(x => Result.Success<float, ValidationError[]>(x + 0.5f))
+					.Data
+					.WithValue(500)
+					.Verify(new object())
+					.TryGetValue()
+					.AssertFailure()
+					.Should()
+					.BeEquivalentTo(_testErrors);
 		}
 	}
 }
